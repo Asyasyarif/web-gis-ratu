79d0009ed3937e5cb171c74a85d131103c2636f8
ARIF <a.syarifudin@windowslive.com>
2024-07-05 17:47:44 +0700

    add data

---
+++ data.js
@@ -0,0 +1,216 @@
+var db = [
+    {
+        rw: "01",
+        tab:[
+            {
+                title: "Kelompok Masyarakat",
+                key : "kelompok_masyarakat",
+                data : [
+                    {
+                        "key": "karang_taruna",
+                        "title": "Karang Taruna",
+                        "value": "1"
+                    },{
+                        "key": "posyandu",
+                        "title": "Posyandu",
+                        "value": "1"
+                    },{
+                        "key": "kelompok_penggajian",
+                        "title": "Kelompok Penggajian",
+                        "value": "1"
+                    }
+                ]
+            },{
+                title: "Ekonomi",
+                key : "ekonomi",
+                data : [
+                    {
+                        "key": "mebel",
+                        "title": "Mebel",
+                        "value": "1 Unit"
+                    },{
+                        "key": "perdangan",
+                        "title": "Perdangan",
+                        "value": "10 Unit"
+                    },{
+                        "key": "warung_makan",
+                        "title": "Warung Makan",
+                        "value": "1 Unit"
+                    },{
+                        "key": "kios_kelontong",
+                        "title": "Kios Kelontong",
+                        "value": "1 Unit"
+                    },{
+                        "key": "toko_swalayan",
+                        "title": "Toko / Swalayan",
+                        "value": "2 Unit"
+                    },{
+                        "key": "percetakan_sablon",
+                        "title": "Percetakan Sablon",
+                        "value": "1 Unit"
+                    },{
+                        "key": "percetakan",
+                        "title": "Percetakan",
+                        "value": "1 Unit"
+                    }
+                ]
+            },{
+                title: "Pendidikan",
+                key: "pendidikan",
+                data : [
+                    {
+                        title : "TK",
+                        key : "tk",
+                        value : "1 Unit"
+                    },{
+                        title: "SD/Sederajat",
+                        key : "sd",
+                        value : "1 Unit"
+                    },{
+                        title: "SLTP/Sederajat",
+                        key : "sltp",
+                        value : "1 Unit"
+                    },{
+                        title: "SLTA/Sederajat",
+                        key : "slta",
+                        value : "1 Unit"
+                    }
+                ]
+            },{
+                title: "Keamanan Dan Kebersihan",
+                key: "keamanan_dan_kebersihan",
+                data : [
+                    {
+                        title : "Pos Kamling",
+                        key : "jumlah_pos_kamling",
+                        value : "1 Unit"
+                    },{
+                        title: "Pos Linmas",
+                        key : "jumlah_pos_linmas",
+                        value : "1 Unit"
+                    },{
+                        title: "Pos Satpam",
+                        key : "jumlah_pos_satpam",
+                        value : "6 Orang"
+                    }
+                ]
+            },{
+                title: "Prasana Air Bersih",
+                key: "prasana_air_bersih",
+                data : [
+                    {
+                        title : "MCK",
+                        key : "jumlah_mck",
+                        value : "1 Unit"
+                    },{
+                        title: "Pengguna PAM",
+                        key : "pengguna_pam",
+                        value : "400 KK"
+                    }
+                ]
+            },{
+                title: "Disabilitas Cacat Mental Dan Fisik",
+                key: "disabilitas_cacat_mental_fisik",
+                data : [
+                    {
+                        title : "Tuna Wicara",
+                        key : "tuna_wicara",
+                        value : "1 Orang"
+                    },{
+                        title : "Tuna Netra",
+                        key : "tuna_netra",
+                        value : "2 Orang"
+                    },
+                ]
+            },{
+                title: "Disabilitas Cacat Mental",
+                key: "disabilitas_cacat_mental",
+                data : [
+                    {
+                        title : "Idiot",
+                        key : "idiot",
+                        value : "2 Orang"
+                    }
+                ]
+            },{
+                title: "Ibadat Dan Olahraga",
+                key: "prasarana_ibadat",
+                data : [
+                    {
+                        title : "Masjid",
+                        key : "jumlah_masjid",
+                        value : "3"
+                    }
+                ]
+            },{
+                title: "Ibadat Dan Olahraga",
+                key: "prasarana_olahraga",
+                data : [
+                    {
+                        title : "Lapangan Bulu Tangkis",
+                        key : "lap_bulu_tangkis",
+                        value : "1"
+                    }, {
+                        title : "Lapangan Bulu Voli",
+                        key : "lap_bulu_voli",
+                        value : "1"
+                    },{
+                        title : "Lapangan Bulu Tenis",
+                        key : "lap_bulu_tenis",
+                        value : "2"
+                    },{
+                        title : "Lapangan Pingpong",
+                        key : "lap_bulu_pingpong",
+                        value : "3"
+                    },
+                ]
+            },{
+                title: "Kesehatan",
+                key: "kesehatan",
+                data : [
+                    {
+                        title : "Posyandu",
+                        key : "posyandu",
+                        value : "1 Unit"
+                    }, {
+                        title : "Toko Obat",
+                        key : "toko_obat",
+                        value : "1 Unit"
+                    },{
+                        title : "Tempat Dokter Praktek",
+                        key : "tempat_dokter_praktek",
+                        value : "1 Unit"
+                    }
+                ]
+            },{
+                title: "Hiburan",
+                key: "hiburan",
+                data : [
+                    {
+                        title : "Hotel Melati",
+                        key : "hotel_melati",
+                        value : "1 Buah"
+                    }, {
+                        title : "Restoran",
+                        key : "restoran",
+                        value : "1 Buah"
+                    }
+                ]
+            },{
+                title: "Lapduk",
+                key: "lapduk",
+                data : [
+                    {
+                        title : "Laki Laki",
+                        key : "l",
+                        value : 945
+                    },{
+                        title : "Perempuan",
+                        key : "p",
+                        value : 931
+                    }
+                ]
+            }
+        ]
+    }
+]
